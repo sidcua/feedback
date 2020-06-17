c5c457a6d2c015975922cf65cf99bee6bf67151c
sidcua <sidcua@gmail.com>
2020-06-17 16:49:12 +0800

    update

---
+++ database/seeds/AccountSeeder.php
@@ -0,0 +1,24 @@
+<?php
+
+use Illuminate\Database\Seeder;
+use App\Models\Account;
+
+class AccountSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        Account::create([
+            'name' => 'Sid Jeric Cua',
+            'email' => 'sidcua@gmail.com',
+            'password' => bcrypt('sidcua'),
+            'type' => 1,
+            'created_at' => date('Y-m-d H:i:s'),
+            'updated_at' => date('Y-m-d H:i:s'),
+        ]);
+    }
+}
